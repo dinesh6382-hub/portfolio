14bbea493e22e78ab50956c34bb51ef944f10224
dinesh6382-hub <devilkd212000@gmail.com>
2025-07-10 18:23:38 +0530

    third commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
       </div>
 
       {/* <ComputersCanvas /> */}
-      <div className='absolute top-[850px] lg:top-[1100px] w-full flex justify-center items-center'>
+      <div className='absolute top-[750px] lg:top-[1100px] w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-1'>
             <motion.div
